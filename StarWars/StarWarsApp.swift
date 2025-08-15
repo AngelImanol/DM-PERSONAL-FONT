424ba021ad662b6c1775c1b356670afe26ab3f23
angelvn <angel.villegas>
2025-08-15 17:33:06 -0600

    Initial Commit

---
+++ StarWars/StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by WIN603 on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
